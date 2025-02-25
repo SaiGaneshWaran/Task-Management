bcc68820d8fbb67c08b9be94eb7cfeec0ccaf489
Saiganeshwaran <saiganeshwaran@gmail.com>
2025-02-25 21:49:37 +0530

    8

---
+++ task-management/src/components/TaskCard.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const TaskCard = ({ task }) => {
+    const handleDragStart = (e) => {
+        e.dataTransfer.setData('taskId', task.id);
+    };
+
+    return (
+        <div className="task-card" draggable onDragStart={handleDragStart}>
+            <h3>{task.title}</h3>
+            <p>{task.description}</p>
+        </div>
+    );
+};
+
+export default TaskCard;
